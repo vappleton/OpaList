158b044467c86f1d4fc8feaa98028a9f13b57a0d
vappleton <139707137+vappleton@users.noreply.github.com>
2026-05-13 20:24:28 -0700

    remove unnecessary codeobsolete

--- src/main/java/com/vappleto/todo/TaskManager.java
+++ src/main/java/com/vappleto/todo/TaskManager.java
@@ -25,40 +25,7 @@ public class TaskManager {
         return tasks;
     }
 
-    public void displayTasks() {
-        for (TaskCategory category : TaskCategory.values()) {
-            System.out.println(category + " Tasks:");
-            List<Task> taskList = tasks.getOrDefault(category, new ArrayList<>());
-            if (taskList.isEmpty()) {
-                System.out.println("No tasks in this category.");
-            } else {
-                for (int i = 0; i < taskList.size(); i++) {
-                    System.out.println((i + 1) + ". " + taskList.get(i));
-                }
-            }
-            System.out.println();
-        }
-    }
 
-    public void markTaskComplete(TaskCategory category, int index) {
-        List<Task> taskList = tasks.get(category);
-        if (taskList != null && index >0 && index <= taskList.size()) {
-            taskList.get(index -1).markCompleted();
-            System.out.println("Task marked as complete!");
-        } else {
-            System.out.println("Invalid task number");
-        }
-
-    }
-    public void removeTask(TaskCategory category, int index) {
-        List<Task> taskList = tasks.get(category);
-        if (taskList != null && index >0 && index <= taskList.size()){
-            taskList.remove(index -1);
-            System.out.println("Task removed");
-        }else {
-            System.out.println("Invalid task number");
-        }
-    }
     public List<Task> getTaskByCategory(TaskCategory category) {
         return tasks.getOrDefault( category, new ArrayList());
     }
